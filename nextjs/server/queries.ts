1d624b23ce423f50b58d46672fc5143b405ab6ec
kanav9063 <kanavarora9063@gmail.com>
2025-05-28 23:56:36 -0700

    set up basic pages

--- nextjs/server/queries.ts
+++ nextjs/server/queries.ts
@@ -8,7 +8,6 @@ import { eq } from "drizzle-orm";
 export function getProjectsForUser(): Promise<Project[]> {
   // Figure out who the user is
   const { userId } = auth();
-  console.log(userId);
 
   // Verify the user exists
   if (!userId) {
@@ -23,3 +22,23 @@ export function getProjectsForUser(): Promise<Project[]> {
 
   return projects;
 }
+
+export async function getProject(projectId: string) {
+  // Figure out who the user is
+  const { userId } = auth();
+
+  // Verify the user exists
+  if (!userId) {
+    throw new Error("User not found");
+  }
+
+
+  // we are checking 2 things here : 1) the projectId matches the id of the project 2) auth : the userId matches the userId of the project
+  const project = await db.query.projectsTable.findFirst({
+    where: (project, { eq, and }) =>
+      and(eq(project.id, projectId), eq(project.userId, userId)),
+  });
+
+  return project;
+}
+
